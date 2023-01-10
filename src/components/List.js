06fd40ee7a5a615367dade6e012fdfebf8959e2c
choiseongyun <5songyon5@naver.com>
2023-01-10 22:13:09 +0900

    List.js dmd commit

--- src/components/List.js
+++ src/components/List.js
@@ -34,9 +34,24 @@ export default function List({ todoData, setTodoData }) {
         setTodoData(newTodoData);
         // this.setState({ todoData: newTodoData });
     };
+    const handleEnd = (result) =>{
+        // result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함
+        if(!result.destination) return;
+
+        // 리액트 불변성을 지켜주기 위해 새로운 todoData 생성
+        const newTodoData =todoData;
+
+        // 1. 변경시키는 아이템을 배열에서 지움
+        // 2. return 값으로 지워진 아이템을 잡아줌
+        const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+        //원하는 자리에 reorderedItem을 insert
+        newTodoData.splice(result.destination.index, 0, reorderedItem);
+        setTodoData(newTodoData);
+    }
   return (
     <div>
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleEnd}>
         <Droppable droppableId='todo'>
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
@@ -47,8 +62,14 @@ export default function List({ todoData, setTodoData }) {
               index={index}
             >
               {(provided, snapshot)=>(
-                <div key={data.id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}>
-                  <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
+              <div key={data.id} 
+                {...provided.draggableProps} 
+                ref={provided.innerRef} 
+                {...provided.dragHandleProps}
+                className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100" 
+                } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
+              >
+                 
                     <div className='items-center'>
                     <input 
                       type="checkbox" 
@@ -60,11 +81,12 @@ export default function List({ todoData, setTodoData }) {
                         <div className='items-center'>
                           <button className='px-4 py-2 float-right' onClick={()=> handleClick(data.id)}>x</button>
                         </div>
-                      </div>
+               
                   </div>
                 )}
               </Draggable>
             ))}
+            {provided.placeholder}
           </div>
         )}
         </Droppable>
